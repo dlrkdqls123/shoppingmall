fc6c15f5052d21d3271045ef4e7f9ff097456561
dlrkdqls123 <zxcfvg180@naver.com>
2023-04-03 15:17:21 +0900

    2023-04-03

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authRouter = require('./routes/auth');
+const myPageRouter = require('./routes/myPage');
+const productsRouter = require('./routes/products');
+const app = express();
+
+app.get('/', (req, res)=>{ res.send('홈');});
+app.use('/auth', authRouter);
+app.use('/myPage', myPageRouter);
+app.use('/products', productsRouter);
+
+app.listen(3000);
